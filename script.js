fd7b9bb5dcf388d9908f6487561a519ae3a34a43
dipen2000 <dipenchavda2000@gmail.com>
2022-02-15 15:47:50 +0530

    login-signup done

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const toProduct = document.querySelector(".to-product");
+const loginBtn = document.querySelector(".login-btn");
 toProduct.addEventListener("click" , ()=>{
     window.location = "/pages/product/product.html";
+});
+
+loginBtn.addEventListener("click" , ()=>{
+    window.location = "/pages/login-signup/login-signup.html";
 });
