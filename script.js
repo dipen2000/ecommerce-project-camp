add05a54910793b545df2e6daaad16aa70073615
dipen2000 <dipenchavda2000@gmail.com>
2022-02-20 21:55:36 +0530

    cleaned the code a little bit and added favicon

--- script.js
+++
@@ -1,9 +0,0 @@
-const toProduct = document.querySelector(".to-product");
-const loginBtn = document.querySelector(".login-btn");
-toProduct.addEventListener("click" , ()=>{
-    window.location = "/pages/product/product.html";
-});
-
-loginBtn.addEventListener("click" , ()=>{
-    window.location = "/pages/login-signup/login-signup.html";
-});
